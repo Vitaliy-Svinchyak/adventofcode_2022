9298ad995ab509672d459caca6bb9b928a7fb079
e33 <vitaliy.svinchyak@gmail.com>
2022-12-13 19:05:18 +0200

    it was only day 1

--- src/solution/day1.rs
+++ src/solution/day1.rs
@@ -1,5 +1,5 @@
 pub fn solve(input: String) {
-    let result: u32 = input
+    let mut callories: Vec<u32> = input
         .split("\n\n")
         .map(|elf_inventory| {
             elf_inventory
@@ -7,8 +7,9 @@ pub fn solve(input: String) {
                 .map(|calories| calories.parse::<u32>().expect("Cannot parse calories"))
                 .sum()
         })
-        .max()
-        .expect("There is no winner");
+        .collect();
+    callories.sort();
+    let result: u32 = callories.iter().rev().take(3).sum();
 
     dbg!(result);
 }
--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1,10 +1,8 @@
 mod day1;
-mod day2;
 
 pub fn solve(input: String, day: u8) {
     match day {
         1 => day1::solve(input),
-        2 => day2::solve(input),
         _ => panic!("No solution for day {day}"),
     }
 }
