132f9fadea80e940dd2f43ee1ea580b68d35cc8f
e33 <vitaliy.svinchyak@gmail.com>
2022-12-16 22:30:43 +0200

    refactor 6

--- src/solution/day6.rs
+++ src/solution/day6.rs
@@ -1,33 +1,13 @@
 use itertools::Itertools;
 
 pub fn solve(input: String) {
+    let chars = input.chars().collect::<Vec<char>>();
     let needed_length = 14;
-    let mut buffer = vec![];
-    let mut result = 0;
-    for (i, char) in input.chars().enumerate() {
-        if buffer.len() == needed_length {
-            buffer.remove(0);
-        }
-        buffer.push(char);
 
-        if buffer.len() == needed_length && all_unique(&buffer) {
-            result = i + 1;
-            break;
-        }
-    }
-
-    dbg!(result);
-}
-
-fn all_unique(buffer: &[char]) -> bool {
-    for (i, char) in buffer.iter().enumerate() {
-        let found = buffer.iter().find_position(|c| *c == char);
-        if let Some((index, ..)) = found {
-            if index != i {
-                return false;
-            }
-        }
-    }
-
-    true
+    let (index, ..) = chars
+        .windows(needed_length)
+        .enumerate()
+        .find(|(.., buffer)| buffer.iter().all_unique())
+        .unwrap();
+    println!("{}", index + needed_length);
 }
