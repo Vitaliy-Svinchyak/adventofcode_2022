7f46b36bc92e657074553009a1742e6dc4e1e59e
e33 <vitaliy.svinchyak@gmail.com>
2022-12-17 20:48:51 +0200

    refactor day 11

--- src/solution/day11.rs
+++ src/solution/day11.rs
@@ -1,9 +1,9 @@
 use itertools::Itertools;
 
 #[derive(Debug, Copy, Clone)]
-enum Operation {
-    Multiply(Argument),
-    Add(Argument),
+enum Operand {
+    Multiply,
+    Add,
 }
 #[derive(Debug, Copy, Clone)]
 enum Argument {
@@ -13,7 +13,7 @@ enum Argument {
 #[derive(Debug)]
 struct Monkey {
     items: Vec<usize>,
-    operation: Operation,
+    operation: (Operand, Argument),
     test_divisible: usize,
     if_true_throw_to: usize,
     if_false_throw_to: usize,
@@ -23,7 +23,7 @@ struct Monkey {
 impl Monkey {
     fn new(
         items: Vec<usize>,
-        operation: Operation,
+        operation: (Operand, Argument),
         test_divisible: usize,
         if_true_throw_to: usize,
         if_false_throw_to: usize,
@@ -45,12 +45,12 @@ impl Monkey {
         self.items
             .drain(0..)
             .map(|mut item| {
-                item = match self.operation {
-                    Operation::Multiply(argument) => match argument {
+                item = match self.operation.0 {
+                    Operand::Multiply => match self.operation.1 {
                         Argument::Old => item * item,
                         Argument::Number(n) => item * n,
                     },
-                    Operation::Add(argument) => match argument {
+                    Operand::Add => match self.operation.1 {
                         Argument::Old => item + item,
                         Argument::Number(n) => item + n,
                     },
@@ -73,7 +73,7 @@ impl Monkey {
 
 pub fn solve(input: String) {
     let mut monkeys: Vec<Monkey> = input.split("\n\n").map(parse_monkey).collect();
-    let common_factor: usize = monkeys.iter().map(|m| m.test_divisible).product();
+    let common_factor: usize = monkeys.iter().map(|monkey| monkey.test_divisible).product();
 
     let mut buffer = vec![vec![]; monkeys.len()];
     for _round in 0..10000 {
@@ -96,16 +96,16 @@ pub fn solve(input: String) {
 }
 
 fn parse_monkey(monkey: &str) -> Monkey {
-    let description_rows: Vec<&str> = monkey.lines().skip(1).collect();
+    let description: Vec<&str> = monkey.lines().skip(1).collect();
 
-    let items: Vec<usize> = description_rows[0]
+    let items: Vec<usize> = description[0]
         .replace("Starting items: ", "")
         .trim()
         .split(", ")
         .map(|item| item.parse::<usize>().unwrap())
         .collect();
 
-    let clean_operation = description_rows[1].replace("Operation: new = old ", "");
+    let clean_operation = description[1].replace("Operation: new = old ", "");
     let (sign, argument): (&str, &str) =
         clean_operation.split_whitespace().collect_tuple().unwrap();
     let argument = if argument == "old" {
@@ -113,34 +113,19 @@ fn parse_monkey(monkey: &str) -> Monkey {
     } else {
         Argument::Number(argument.parse().unwrap())
     };
-    let operation = if sign == "*" {
-        Operation::Multiply(argument)
+    let operand = if sign == "*" {
+        Operand::Multiply
     } else {
-        Operation::Add(argument)
+        Operand::Add
     };
 
-    let test_divisible = description_rows[2]
-        .rsplit_once(' ')
-        .unwrap()
-        .1
-        .parse()
-        .unwrap();
-    let if_true_throw_to = description_rows[3]
-        .rsplit_once(' ')
-        .unwrap()
-        .1
-        .parse()
-        .unwrap();
-    let if_false_throw_to = description_rows[4]
-        .rsplit_once(' ')
-        .unwrap()
-        .1
-        .parse()
-        .unwrap();
+    let test_divisible = description[2].rsplit_once(' ').unwrap().1.parse().unwrap();
+    let if_true_throw_to = description[3].rsplit_once(' ').unwrap().1.parse().unwrap();
+    let if_false_throw_to = description[4].rsplit_once(' ').unwrap().1.parse().unwrap();
 
     Monkey::new(
         items,
-        operation,
+        (operand, argument),
         test_divisible,
         if_true_throw_to,
         if_false_throw_to,
