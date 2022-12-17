5e5160f13742ede8e8c6dea061368d8a7464b8fe
e33 <vitaliy.svinchyak@gmail.com>
2022-12-17 18:16:00 +0200

    refactor 10

--- src/solution/day10.rs
+++ src/solution/day10.rs
@@ -1,30 +1,23 @@
 pub fn solve(input: String) {
     let mut register = 1;
-    let mut cycle = 0;
+    let mut cycle: isize = 0;
     let cycles_to_check = [20, 60, 100, 140, 180, 220];
     let mut total_signal_strength = 0;
 
-    let mut screen = vec![Vec::with_capacity(40); 6];
-    let mut screen_row = 0;
+    let mut screen = [[" "; 40]; 6];
 
-    for command in input.replace(' ', "\n").lines() {
+    for command in input.split(|b| b == ' ' || b == '\n') {
         cycle += 1;
 
         // drawing
-        let current_pixel = screen[screen_row].len();
+        let current_pixel = ((cycle - 1) % 40) as usize;
         if current_pixel.abs_diff(register as usize) <= 1 {
-            screen[screen_row].push("■");
-        } else {
-            screen[screen_row].push(" ");
-        }
-        if cycle % 40 == 0 {
-            screen_row += 1;
+            let screen_row = (cycle / 40) as usize;
+            screen[screen_row][current_pixel] = "■";
         }
 
-        // registry
         if cycles_to_check.contains(&cycle) {
             let signal_strength = cycle * register;
-            println!("{cycle} * {register} = {signal_strength}");
             total_signal_strength += signal_strength;
         }
 
@@ -38,7 +31,7 @@ pub fn solve(input: String) {
 }
 
 // FBURHZCH
-fn draw(screen: &[Vec<&str>]) {
+fn draw(screen: &[[&str; 40]]) {
     for row in screen {
         println!("{}", row.join(""));
     }
